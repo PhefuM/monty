e983b5a72b5fb50f52b74d2b227bbbbf77c1b3e2
PhefuM <phefumadyo@gmail.com>
2023-08-23 13:29:36 -0700

    main file update

--- swap.c
+++ swap.c
@@ -18,7 +18,7 @@ void f_swap(stack_t **head, unsigned int counter)
 	}
 	if (len < 2)
 	{
-		frpintf(stderr, "L%d: can't swap, stack to short\n", counter);
+		fprintf(stderr, "L%d: can't swap, stack to short\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
